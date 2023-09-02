1aae44324de3b6f328d748ade7fe572738657dfe
RobertCampbel19 <25694106-RobertCampbel19@users.noreply.replit.com>
2023-09-02 21:18:36 +0000

    I was hoping to use double for the values of the coins so that I could also out put Joe's total value, but could really wrap my head around how to do that while also using %= since this cant be used with doubles and int in the same line of calculations from what it seems. As a result I chose to go with whole number values for the coins.

--- .ccls-cache/@home@runner@Add-up-those-coins/main.cpp
+++ .ccls-cache/@home@runner@Add-up-those-coins/main.cpp
@@ -1,8 +1,51 @@
-//
+/*
+Robert Campbell
+COSC - 1436
+This program will convert the number of pennies into the equivilant Dollars, Quarters, Dimes, and Nickles.
+*/
+
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()  
 {
-  std::cout << "Hello World!\n";
+//Variables
+  
+  int Dollars;
+  int Quarters;
+  int Dimes;
+  int Nickles;
+  int Pennies;
+  int Dollars_Value = 100;
+  int Quarters_Value = 25;
+  int Dimes_Value = 10;
+  int Nickles_Value = 5;
+  int Pennies_Value = 1;
+  
+//Input
+  cout << "So, how many pennies do we have today Joe? " ;
+  cin >> Pennies;
+  
+//Proccess(calculations) The value for Pennies will be udated after each calculation to lastly output the reamaing Pennies if any.
+  Dollars = Pennies / Dollars_Value;
+  Pennies %= Dollars_Value;
+  
+  Quarters = Pennies / Quarters_Value;
+  Pennies %= Quarters_Value;
+  
+  Dimes = Pennies / Dimes_Value;
+  Pennies %= Dimes_Value;
+  
+  Nickles = Pennies / Nickles_Value;
+  Pennies %= Nickles_Value;
+
+//Output what would be given to Joe after he trades in his Pennies.
+  cout << "you will receive: \n" << Dollars << " dollars " << endl;
+  cout << Quarters << " quarters " << endl;
+  cout << Dimes << " Dimes " << endl;
+  cout << Nickles << " Nickles " << endl;
+  cout << Pennies << " pennies " << endl;
+
+  return 0;
 }
